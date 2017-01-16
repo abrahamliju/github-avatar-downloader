ad0196915dc6ab5c709167bb731c1a8503cb6262
Liju <abrahamliju@live.com>
2017-01-16 22:49:05 +0000

    joining the repoName and repoOwner  name together

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,12 @@
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
+var gitHub_User = "abrahamliju";
+var gitHub_Token = "597384a2f9ad7d8374aa18f916f0c31cac958368";
 
-fuction getRepoContributors(repoOwner, repoName,cb){
+function getRepoContributors(repoOwner, repoName,cb){
+  var requestURL = 'https://'+ gitHub_User + ':' + gitHub_Token + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+
 //request.get("")
 
 
@@ -12,3 +17,9 @@ getRepoContributors("jquery","jquery", function(err, result){
   console.log("Errors:", err);
   console.log("Result:", result);
 })
+
+
+
+
+
+
