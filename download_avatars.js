4b013c36af3170eb18a2042d6fd0694dc108681a
Liju <abrahamliju@live.com>
2017-01-16 22:36:30 +0000

    Added func to call the repo and reponame and to handle errors

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,14 @@
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+fuction getRepoContributors(repoOwner, repoName,cb){
+//request.get("")
+
+
+
+}
+
+getRepoContributors("jquery","jquery", function(err, result){
+  console.log("Errors:", err);
+  console.log("Result:", result);
+})
