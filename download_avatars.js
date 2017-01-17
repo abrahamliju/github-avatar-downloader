9c60043a067679b12351e73c217d7d54391f6fa1
Liju <abrahamliju@live.com>
2017-01-17 00:20:02 +0000

    Printing each avatarUrl from the repo

--- download_avatars.js
+++ download_avatars.js
@@ -3,19 +3,31 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 var gitHub_User = "abrahamliju";
 var gitHub_Token = "597384a2f9ad7d8374aa18f916f0c31cac958368";
 
-function getRepoContributors(repoOwner, repoName,cb){
-  var requestURL = 'https://'+ gitHub_User + ':' + gitHub_Token + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
 
-//request.get("")
-
+function getRepoContributors(repoOwner, repoName, cb){
+  var data
+  var requestURL = {
+     url: 'https://'+ gitHub_User + ':' + gitHub_Token + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+     headers: {
+    'User-Agent': 'GitHub Avatar Downloader - Student Project'
+  }
+  };
+  // console.log(requestURL);
+  request(requestURL, cb);
 
 
 }
 
-getRepoContributors("jquery","jquery", function(err, result){
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors("jquery","jquery", function(err, result,body){
+
+  var data = JSON.parse(body);
+
+
+  let avatarUrls =
+    data.map(item => item.avatar_url)
+    console.log(avatarUrls);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
 })
 
 
