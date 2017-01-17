2cdfa99bee65cfc2fdffe8e79214dd1933cd10ce
Liju <abrahamliju@live.com>
2017-01-17 01:00:30 +0000

    Downloading Avatars in Avatar folder

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 var gitHub_User = "abrahamliju";
 var gitHub_Token = "597384a2f9ad7d8374aa18f916f0c31cac958368";
@@ -21,14 +22,23 @@ function getRepoContributors(repoOwner, repoName, cb){
 getRepoContributors("jquery","jquery", function(err, result,body){
 
   var data = JSON.parse(body);
-
-
   let avatarUrls =
-    data.map(item => item.avatar_url)
+    data.map(item => item.avatar_url);
+  let userIds =
+    data.map(item => item.login);
     console.log(avatarUrls);
-  // console.log("Errors:", err);
-  // console.log("Result:", result);
-})
+  for(var i = 0; i < avatarUrls.length; i++){
+    downloadImageByUrl(avatarUrls[i],"avatars/"+userIds[i]+".jpg");
+  }
+});
+
+function downloadImageByUrl(url, filepath){
+
+  request.get(url)
+    .pipe(fs.createWriteStream(filepath));
+}
+
+//downloadImageByUrl("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 
 
